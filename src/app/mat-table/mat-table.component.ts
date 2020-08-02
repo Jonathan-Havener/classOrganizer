fc5458a147e040670e5407dfe8359e21bd4d84d6
Locodays <jon.a.havener@gmail.com>
2020-08-02 17:29:39 -0400

    Added download file functionality

--- src/app/mat-table/mat-table.component.ts
+++ src/app/mat-table/mat-table.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import {MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
-import { Papa, ParseResult } from 'ngx-papaparse'
+import { Papa, ParseResult } from 'ngx-papaparse';
+import * as fileSaver from 'file-saver';
+import { FormGroup, FormControl } from '@angular/forms';
 
 
 @Component({
@@ -13,26 +15,22 @@ import { Papa, ParseResult } from 'ngx-papaparse'
 export class MatTableComponent implements OnInit {
 
   fileToUpload: File = null;
-
   tableDataSrc: any;
+  uploadForm = new FormGroup({
+    file: new FormControl(),
+    submit: new FormControl()
+  });
+
   @Input('tableColumns') tableCols: string[] = ["name"];
   @Input() tableData: {}[] = [
-    
   ];
 
 
 
   @ViewChild(MatSort, {static : true}) sort: MatSort;
   @ViewChild(MatPaginator, {static : true}) paginator: MatPaginator;
-
   constructor(private papa: Papa) {
-    const csvData = '"Hello","World!"';
     
-    this.papa.parse(csvData,{
-        complete: (result) => {
-            console.log('Parsed: ', result);
-        }
-    });
 }
 
   ngOnInit(): void {
@@ -47,11 +45,13 @@ export class MatTableComponent implements OnInit {
   }
 
   handleFileInput(files:FileList){
+
     this.fileToUpload = files.item(0);
     this.papa.parse(files.item(0), {
       complete : (results) =>{
         return new Promise((resolve,reject) =>{
-          console.log("The results are : "+results);
+          
+          //console.log("The results are : "+results);
           results.data.forEach(element => {
             if(!this.tableDataSrc.data.some(el =>  el.name == element[0])){
               this.tableDataSrc.data.push({name:element[0]})
@@ -62,13 +62,38 @@ export class MatTableComponent implements OnInit {
           });
           resolve()
         }).then( ()=>{
-          console.log(this.tableDataSrc.data);
+          //console.log(this.tableDataSrc.data);
           //reassigning tableDataSrc.data forces it to update
           this.tableDataSrc.data = this.tableDataSrc.data
         } )
         
       } 
     })
+  }
+
+  downloadCSV(){
+
+    var csvData
+    return new Promise((resolve, reject)=>{
+      csvData = new Blob([ this.papa.unparse(this.tableDataSrc.data) ])
+      
+      if(csvData!= null){
+        console.log(csvData)
+        resolve()
+      }
+      else{
+        console.log("Rejected")
+        reject();
+      }
+    }).then(()=>{
+      console.log(csvData)
+      fileSaver.saveAs(csvData, "test.csv")
+    })
+    
+  }
+
+  test(){
+    console.log(this.uploadForm)
   }
 
 }
